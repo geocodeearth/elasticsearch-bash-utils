8a3bc917e3783de15e233d568a2af570384a6f04
Julian Simioni <julian@simioni.org>
2020-02-10 17:30:11 -0800

    Upgrade script

--- load_snapshot.sh
+++ load_snapshot.sh
@@ -8,12 +8,24 @@ set -euo pipefail
 
 cluster_url="${cluster_url:-http://localhost:9200}"
 base_path=${base_path:-elasticsearch}
-es_repo_name="pelias_snapshots"
-s3_bucket="${s3_bucket:-geocodeearth-mapzen-elasticsearch-snapshots}"
-new_snapshot_name="${new_snapshot_name:-pelias-2017.11.18-001123}"
+es_repo_name="pelias_snapshot"
+s3_bucket="${s3_bucket:-}"
+snapshot_name="${snapshot_name:-}"
 read_only="${read_only:-true}"
 
-# create bucket, only needs to be run once
+# check all required variables are set
+if [[ "$s3_bucket" == "" ]]; then
+  echo "s3_bucket not set, no snapshot will be loaded"
+  exit 1
+fi
+
+# check jq is installed
+if ! [[ -x "$(command -v jq)" ]]; then
+  echo 'Error: jq is not installed.' >&2
+  exit 1
+fi
+
+# create elasticsearch snapshot repository
 curl -XPOST "$cluster_url/_snapshot/$es_repo_name" \
   -H 'Content-Type: application/json' \
   -d "{
@@ -27,11 +39,20 @@ curl -XPOST "$cluster_url/_snapshot/$es_repo_name" \
  }
 }"
 
-## import new snapshot with name including timestamp
-curl -XPOST "$cluster_url/_snapshot/$es_repo_name/${new_snapshot_name}/_restore" \
+## autodetect snapshot name if not specified
+if [[ "$snapshot_name" == "" ]]; then
+	snapshot_name=$(curl -s -XGET --fail "$cluster_url/_snapshot/$es_repo_name/_all" | jq -r .snapshots[0].snapshot)
+	echo "autodetected snapshot name is $snapshot_name"
+fi
+
+## import new snapshot
+## The resulting index name will include the full snapshot
+curl -XPOST "$cluster_url/_snapshot/$es_repo_name/$snapshot_name/_restore" \
   -H 'Content-Type: application/json' \
   -d "{
   \"indices\": \"pelias\",
   \"rename_pattern\": \"pelias\",
-  \"rename_replacement\": \"$new_snapshot_name\"
+  \"rename_replacement\": \"$snapshot_name\"
 }"
+
+echo "loading snapshot into index $snapshot_name"
